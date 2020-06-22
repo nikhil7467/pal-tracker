d99d012512bf6425dbcf7de32865887c39e6e718
nikhil7467 <nikhil7467@gmail.com>
2020-06-22 10:52:30 +0100

    setup the env

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String varValue;
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+
+        return varValue;
+    }
+
+    public WelcomeController(@Value("${welcome.message}") String varValue) {
+        this.varValue = varValue;
     }
 }
